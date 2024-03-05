3348de3f2342f42620dd426385f1cc4f2f01ca7b
Gerardo Montini <germontini@hotmail.com>
2024-03-05 18:44:35 -0300

    metadescripcion

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
@@ -9,13 +9,10 @@ import { RouterLink } from '@angular/router';
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
 })
-export class MainPage implements OnInit {
+export class MainPage  {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  
 }
