1653d66a78ecefd22455a7a41c682240b72bdbf4
Gerardo Montini <germontini@hotmail.com>
2024-03-05 16:38:56 -0300

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes= [
-  { path: 'main', loadComponent: ()=> import('./main/main.page').then(m =>m.MainPage)},
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./main/main.page').then((m) => m.MainPage),
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./main/main.page').then((m) => m.MainPage),
+  },
   {
     path: 'product-list',
-    loadComponent: () => import('./product-list/product-list.page').then( m => m.ProductListPage)
-  }
+    loadComponent: () =>
+      import('./product-list/product-list.page').then((m) => m.ProductListPage),
+  },
 ];
 export default routes;
